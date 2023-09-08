2cf6c4fc1b81ab8745c754fe9f75dee96b1ec357
HP <HP@DESKTOP-7NBNR9A>
2023-09-08 10:36:27 +0530

    done

---
+++ src/Demo/Sample.java
@@ -0,0 +1,16 @@
+
+package Demo;
+
+public class Sample {
+
+	public static void main(String[] args) {
+	//	System.out.println("Hello World");
+	/*	System.out.println("Java.....!!!");*/
+		System.out.print("PLATFORM INDEPENDENT.....");
+		System.out.println("123");
+		System.out.println(2+3);
+		
+		
+			}
+
+}
